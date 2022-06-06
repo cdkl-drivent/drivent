41c4789c7daa5754adc273d36cd0868ef03a7b2c
demani.lukas@gmail.com <demani.lukas@gmail.com>
2022-06-06 20:08:08 +0100

    feat: payment confirmation

--- src/components/FinalizeButtons/FinalizeButtons.js
+++ src/components/FinalizeButtons/FinalizeButtons.js
@@ -1,12 +1,19 @@
+import usePayment from '../../hooks/usePayment';
 import useReserve from '../../hooks/useReserve';
 import { SCFinalizeButtons } from './styled';
 
 export default function FinalizeButtons() {
   const { setReserve } = useReserve();
+  const { setPayment } = usePayment();
+
+  function handleFinalize() {
+    setPayment(true);
+  };
+
   return (
     <SCFinalizeButtons>
       <button onClick={() => setReserve(false)}>Voltar</button>
-      <button onClick={() => alert('função finalizar')}>Finalizar Pagamento</button>
+      <button onClick={handleFinalize}>Finalizar Pagamento</button>
     </SCFinalizeButtons>
   );
 }
--- src/components/FinalizePayment/FinalizePayment.js
+++ src/components/FinalizePayment/FinalizePayment.js
@@ -2,13 +2,27 @@ import AddCard from '../AddCard/AddCard';
 import ChosenTicket from '../ChosenTicket/ChosenTicket';
 import FinalizeButtons from '../FinalizeButtons/FinalizeButtons';
 import { SCFinalizePayment } from './styled';
+import usePayment from '../../hooks/usePayment';
+import ConfirmationMsg from '../ConfirmationMsg/ConfimationMsg';
 
 export default function FinalizePayment({ orderData }) {
+  const { payment } = usePayment();
   return (
     <SCFinalizePayment>
       <ChosenTicket {...orderData} />
-      <AddCard />
-      <FinalizeButtons />
+      {!payment ? 
+        (
+          <>
+            <AddCard />
+            <FinalizeButtons />
+          </>
+        )
+        :
+        (
+          <ConfirmationMsg />
+        )
+      }
+      
     </SCFinalizePayment>
   );
 }
